68382bd3c3c508295795cb8a7cdc34c1f0ea98dc
BiaAkemi <beancaakemi@gmail.com>
2023-11-13 13:24:30 -0300

    first commit

---
+++ Ex01EncontreNo.java
@@ -0,0 +1,33 @@
+package JavaExercicios.Modulo11VetoresMatrizes;
+
+import java.util.Scanner;
+
+public class Ex01EncontreNo {
+
+	public static void main(String[] args) {
+		
+		int num, x;
+		boolean procura = false;
+		Scanner leia = new Scanner (System.in);
+		
+		
+		int vetor [] = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
+		
+		System.out.println("Digite o número que você deseja encontrar");
+		num = leia.nextInt();
+		
+		for (x=0;x<vetor.length;x++) {
+			
+			if(num == vetor[x]) {
+				
+				procura = true;
+				System.out.println("\n O número "+num+" está na posição "+x);	
+			}
+			}
+			
+		if(! procura) {
+			System.out.println("\n O número "+num+" não foi encontrado!");
+		}
+	}
+
+}
---
+++ Ex04Notas.java
@@ -0,0 +1,35 @@
+package JavaExercicios.Modulo11VetoresMatrizes;
+
+import java.util.Scanner;
+
+public class Ex04Notas {
+
+	public static void main(String[] args) {
+		
+		int linha, coluna;
+		double soma = 0;
+		double matriz [] [] = new double [10][4];
+		double vetorMedia [] = new double[10];
+		
+		Scanner ler = new Scanner(System.in);
+		
+		for(linha=0; linha<=9; linha++) {
+			for(coluna=0; coluna<=3; coluna++) {
+				
+				System.out.println("Entre com a "+(coluna+1)+" nota: ");
+				matriz[linha][coluna] = ler.nextDouble();	
+				
+				soma += matriz[linha][coluna];
+				
+			} vetorMedia [linha] = soma/4;
+			soma=0;
+		}
+		
+		for(double num: vetorMedia) {
+			
+			System.out.printf("\n %.1f",num);
+		}
+						
+	}
+
+}
